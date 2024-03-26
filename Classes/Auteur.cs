af659e9e321f561b38ded8827e8e05421935c296
Marion <Marion.R@live.fr>
2024-03-26 18:24:16 +0100

    Création des classes (sauf employés)

---
+++ Classes/Auteur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mots_Merveilles.Classes
+{
+    /// <summary>
+    /// Classe Auteur
+    /// </summary>
+    public class Auteur
+    {
+        private int idAuteur;
+        private string nom;
+        private string prenom;
+
+        /// <summary>
+        /// Constructeur de la classe Auteur
+        /// </summary>
+        /// <param name="idAuteur"></param>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        public Auteur(int idAuteur, string nom, string prenom)
+        {
+            this.idAuteur = idAuteur;
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        public int GetIdAuteur() { return this.idAuteur; }
+        public void SetIdAuteur(int idAuteur) { this.idAuteur = idAuteur; }
+        public string GetNom() { return this.nom; }
+        public void SetNom(string nom) { this.nom = nom; }
+        public string GetPrenom() { return this.prenom; }
+        public void SetPrenom(string prenom) { this.prenom = prenom; }
+        public override string ToString() { return this.nom + " " + this.prenom;}
+    }
+}
---
+++ Classes/GroupeUtilisateur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mots_Merveilles.Classes
+{
+    /// <summary>
+    /// Classe GroupeUtilisateur
+    /// </summary>
+    public class GroupeUtilisateur
+    {
+        private int idGroupeUtilisateur;
+        private string nom;
+        private bool estAdmin;
+
+        /// <summary>
+        /// Constructeur de la classe GroupeUtilisateur
+        /// </summary>
+        /// <param name="idGroupeUtilisateur"></param>
+        /// <param name="nom"></param>
+        /// <param name="estAdmin"></param>
+        public GroupeUtilisateur(int idGroupeUtilisateur, string nom, bool estAdmin)
+        {
+            this.idGroupeUtilisateur = idGroupeUtilisateur;
+            this.nom = nom;
+            this.estAdmin = estAdmin;
+        }
+
+        public void SetIdGroupeUtilisateur(int idGroupeUtilisateur) { this.idGroupeUtilisateur = idGroupeUtilisateur; }
+        public int GetIdGroupeUtilisateur() { return this.idGroupeUtilisateur;}
+        public void SetNom(string nom) { this.nom = nom;}
+        public string GetNom() { return this.nom;}
+        public void SetEstAdmin(bool estAdmin) { this.estAdmin = estAdmin;}
+        public bool GetEstAdmin() { return this.estAdmin;}
+        public override string ToString() { return this.nom;}
+    }
+}
